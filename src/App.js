d386cd1263eab4ffd68cd5cfd8aeda028b6d8f4e
ambili-nv <ambilinadvilvalappil@gmail.com>
2024-03-28 12:53:59 +0530

    modified - netflix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import './App.css'
+import {action,orginals} from './urls'
+import Banner from "./components/Banner/Banner";
+import RowPost from "./components/RowPost/RowPost";
+import NavBar from "./components/NavBar/NavBar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <Banner/>
+      <RowPost url = {orginals} title = 'Netflix Orginals'/>
+      <RowPost url = {action} title = 'Actions' isSmall/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY = '284aa72c1a5ba9e014ebdac9806777c7';
+
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
